1f3abf7b621e6ddf737d12ab295c368e5f2b1c05
Will Angley <willangley@google.com>
2021-12-01 11:56:20 -0500

    [2021] Rename `day_1` to `sonar_sweep`.

--- 2021/sonar_sweep_test.py
+++ 2021/sonar_sweep_test.py
@@ -1,9 +1,11 @@
+"""Tests for Day 1: Sonar Sweep."""
+
 import unittest
 
-import day_1
+import sonar_sweep
 
 
-class MyTestCase(unittest.TestCase):
+class SonarSweepTestCase(unittest.TestCase):
   def setUp(self):
     self.raw_report = """
     199
@@ -17,26 +19,27 @@ class MyTestCase(unittest.TestCase):
     260
     263
     """
-    self.report = day_1.parse_report(self.raw_report)
+    self.report = sonar_sweep.parse_report(self.raw_report)
 
   def test_parse_report(self):
-    self.assertListEqual(day_1.parse_report(self.raw_report),
+    self.assertListEqual(sonar_sweep.parse_report(self.raw_report),
                          [199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
 
   def test_pairwise(self):
     self.assertListEqual(['AB', 'BC', 'CD', 'DE', 'EF', 'FG'],
-                         [''.join(pair) for pair in day_1.pairwise('ABCDEFG')])
+                         [''.join(pair) for pair in
+                          sonar_sweep.pairwise('ABCDEFG')])
 
   def test_count_depth_increases(self):
-    self.assertEqual(day_1.count_depth_increases(self.report), 7)
+    self.assertEqual(sonar_sweep.count_depth_increases(self.report), 7)
 
   def test_threewise(self):
     self.assertListEqual(['ABC', 'BCD', 'CDE', 'DEF', 'EFG'],
                          [''.join(triple) for triple in
-                          day_1.threewise('ABCDEFG')])
+                          sonar_sweep.threewise('ABCDEFG')])
 
   def test_count_depth_increases_windowed(self):
-    self.assertEqual(day_1.count_depth_increases_windowed(self.report), 5)
+    self.assertEqual(sonar_sweep.count_depth_increases_windowed(self.report), 5)
 
 
 if __name__ == '__main__':
