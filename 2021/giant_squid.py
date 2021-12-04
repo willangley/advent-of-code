bdea6ec6ad7516ec03231a8efbf53dfe103d632d
Will Angley <will@willangley.org>
2021-12-04 17:05:16 -0500

    [2021][Day 4] Part one: bingo game!

--- 2021/giant_squid.py
+++ 2021/giant_squid.py
@@ -11,12 +11,17 @@ import sys
 def parse_input(raw_input: str):
   draws = []
   boards = []
+  marks = []
+
   current_board = []
 
   def finish_board():
-    nonlocal boards, current_board
+    nonlocal boards, current_board, marks
     if len(current_board):
       boards.append(current_board)
+      ii, jj = len(current_board), len(current_board[0])
+      marks.append([[False for _ in range(jj)]
+                    for _ in range(ii)])
       current_board = []
 
   for i, line in enumerate(raw_input.strip().splitlines()):
@@ -32,7 +37,46 @@ def parse_input(raw_input: str):
   else:
     finish_board()
 
-  return draws, boards
+  return draws, boards, marks
+
+
+def play_bingo_draw(draw, boards, marks):
+  for i in range(len(boards)):
+    for j in range(len(boards[i])):
+      for k in range(len(boards[i][j])):
+        if boards[i][j][k] != draw:
+          continue
+
+        marks[i][j][k] = True
+
+        winning_row = True
+        winning_col = True
+        for jj in range(len(boards[i])):
+          winning_col &= marks[i][jj][k]
+        for kk in range(len(boards[i][j])):
+          winning_row &= marks[i][j][kk]
+
+        if winning_row or winning_col:
+          return i
+
+  return None
+
+
+def play_bingo_game(draws, boards, marks):
+  score = 0
+  for draw in draws:
+    winning_board_index = play_bingo_draw(draw, boards, marks)
+    if winning_board_index is not None:
+      winning_board = boards[winning_board_index]
+      winning_marks = marks[winning_board_index]
+
+      for i in range(len(winning_board)):
+        for j in range(len(winning_board[i])):
+          if not winning_marks[i][j]:
+            score += winning_board[i][j]
+
+      score *= draw
+      return score
 
 
 if __name__ == "__main__":
@@ -41,6 +85,5 @@ if __name__ == "__main__":
                       default=sys.stdin)
   args = parser.parse_args()
 
-  draws, boards = parse_input(args.infile.read())
-  print(draws)
-  print(boards)
+  draws, boards, marks = parse_input(args.infile.read())
+  print('[Part 1] score:', play_bingo_game(draws, boards, marks))
