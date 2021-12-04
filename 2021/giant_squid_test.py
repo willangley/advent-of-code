764e53eff645fc0f49ce0c62d15854233ed596ca
Will Angley <will@willangley.org>
2021-12-04 17:05:16 -0500

    [2021][Day 4] Part one: bingo game!

--- 2021/giant_squid_test.py
+++ 2021/giant_squid_test.py
@@ -29,7 +29,7 @@ class GiantSquidTestCase(unittest.TestCase):
 """
 
   def test_parse_input(self):
-    draws, boards = giant_squid.parse_input(self.raw_input)
+    draws, boards, _ = giant_squid.parse_input(self.raw_input)
     self.assertListEqual(draws,
                          [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13,
                           6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1])
@@ -40,6 +40,11 @@ class GiantSquidTestCase(unittest.TestCase):
          [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
         [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20],
          [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]])
+
+  def test_play_bingo_game(self):
+    score = giant_squid.play_bingo_game(
+      *giant_squid.parse_input(self.raw_input))
+    self.assertEqual(4512, score)
 
 
 if __name__ == '__main__':
