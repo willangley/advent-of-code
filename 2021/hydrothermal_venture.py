7a684331b59073d7082a6783f7da9a5b7f70dca0
Will Angley <willangley@google.com>
2021-12-05 17:22:31 -0500

    [2021][Day 5] Part one: horizontal and vertical lines.

--- 2021/hydrothermal_venture.py
+++ 2021/hydrothermal_venture.py
@@ -5,6 +5,8 @@ https://adventofcode.com/2021/day/5
 """
 
 import argparse
+from enum import Enum
+import itertools
 import re
 import sys
 from typing import List, NamedTuple
@@ -15,6 +17,15 @@ class Line(NamedTuple):
   y1: int
   x2: int
   y2: int
+
+
+class Point(NamedTuple):
+  x: int
+  y: int
+
+
+class Part(Enum):
+  ONE = 1
 
 
 def parse_line(raw_line: str) -> Line:
@@ -28,6 +39,42 @@ def parse_input(raw_input: str) -> List[Line]:
   return [parse_line(line) for line in raw_input.strip().splitlines()]
 
 
+Diagram = List[List[int]]
+
+
+def points(line: Line) -> List[Point]:
+  if line.x1 == line.x2:
+    step = 1 if line.y2 >= line.y1 else -1
+    return [Point(line.x1, y) for y in range(line.y1, line.y2 + step, step)]
+  elif line.y1 == line.y2:
+    step = 1 if line.x2 >= line.x1 else -1
+    return [Point(x, line.y1) for x in range(line.x1, line.x2 + step, step)]
+
+
+def draw_diagram(lines: List[Line], part: Part = Part.ONE) -> Diagram:
+  if part == Part.ONE:
+    lines = [line for line in lines if line.x1 == line.x2 or line.y1 == line.y2]
+
+  max_x = max(itertools.chain(*((line.x1, line.x2) for line in lines)))
+  max_y = max(itertools.chain(*((line.y1, line.y2) for line in lines)))
+
+  diagram = [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+  for line in lines:
+    for point in points(line):
+      diagram[point.y][point.x] += 1
+
+  return diagram
+
+
+def score_diagram(diagram: Diagram) -> int:
+  return sum(sum(1 for c in row if c > 1) for row in diagram)
+
+
+def print_diagram(diagram: Diagram) -> str:
+  return '\n'.join(
+      ''.join('.' if c == 0 else str(c) for c in row) for row in diagram)
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Day 5: Hydrothermal Venture")
   parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
@@ -35,4 +82,4 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   lines = parse_input(args.infile.read())
-  print(lines)
+  print('[Part 1] score:', score_diagram(draw_diagram(lines)))
--- 2021/hydrothermal_venture_test.py
+++ 2021/hydrothermal_venture_test.py
@@ -4,6 +4,7 @@
 import unittest
 
 import hydrothermal_venture
+from hydrothermal_venture import Line, Point
 
 
 class TestCase(unittest.TestCase):
@@ -20,23 +21,49 @@ class TestCase(unittest.TestCase):
 0,0 -> 8,8
 5,5 -> 8,2
 """
+    self.lines = hydrothermal_venture.parse_input(self.raw_input)
 
   def test_parse_line(self):
-    self.assertEqual(hydrothermal_venture.Line(0, 9, 5, 9),
+    self.assertEqual(Line(0, 9, 5, 9),
                      hydrothermal_venture.parse_line("0,9 -> 5,9"))
 
   def test_parse_input(self):
-    self.assertListEqual([hydrothermal_venture.Line(0, 9, 5, 9),
-                          hydrothermal_venture.Line(8, 0, 0, 8),
-                          hydrothermal_venture.Line(9, 4, 3, 4),
-                          hydrothermal_venture.Line(2, 2, 2, 1),
-                          hydrothermal_venture.Line(7, 0, 7, 4),
-                          hydrothermal_venture.Line(6, 4, 2, 0),
-                          hydrothermal_venture.Line(0, 9, 2, 9),
-                          hydrothermal_venture.Line(3, 4, 1, 4),
-                          hydrothermal_venture.Line(0, 0, 8, 8),
-                          hydrothermal_venture.Line(5, 5, 8, 2)],
-                         hydrothermal_venture.parse_input(self.raw_input))
+    self.assertListEqual([Line(0, 9, 5, 9),
+                          Line(8, 0, 0, 8),
+                          Line(9, 4, 3, 4),
+                          Line(2, 2, 2, 1),
+                          Line(7, 0, 7, 4),
+                          Line(6, 4, 2, 0),
+                          Line(0, 9, 2, 9),
+                          Line(3, 4, 1, 4),
+                          Line(0, 0, 8, 8),
+                          Line(5, 5, 8, 2)],
+                         self.lines)
+
+  def test_points(self):
+    self.assertListEqual([Point(1, 1), Point(1, 2), Point(1, 3)],
+                         hydrothermal_venture.points(Line(1, 1, 1, 3)))
+    self.assertListEqual([Point(9, 7), Point(8, 7), Point(7, 7)],
+                         hydrothermal_venture.points(Line(9, 7, 7, 7)))
+
+  def test_print_diagram(self):
+    self.assertEqual("""
+.......1..
+..1....1..
+..1....1..
+.......1..
+.112111211
+..........
+..........
+..........
+..........
+222111....
+""".strip(), hydrothermal_venture.print_diagram(
+        hydrothermal_venture.draw_diagram(self.lines)))
+
+  def test_score_diagram(self):
+    self.assertEqual(5, hydrothermal_venture.score_diagram(
+        hydrothermal_venture.draw_diagram(self.lines)))
 
 
 if __name__ == '__main__':
