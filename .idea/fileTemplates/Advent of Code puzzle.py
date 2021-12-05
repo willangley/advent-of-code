7e4cfadfe3ea2402c3752d2c77b73b16e9b57607
Will Angley <willangley@google.com>
2021-12-05 16:29:29 -0500

    [2021] Add an IntelliJ template for puzzles.

---
+++ .idea/fileTemplates/Advent of Code puzzle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+--- Day $PUZZLE_DAY: $PUZZLE_NAME ---
+https://adventofcode.com/$PUZZLE_YEAR/day/$PUZZLE_DAY
+"""
+
+import argparse
+import sys
+
+
+def parse_input(raw_input: str):
+  for line in raw_input.strip().splitlines():
+    pass
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Day $PUZZLE_DAY: $PUZZLE_NAME")
+  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
+                      default=sys.stdin)
+  args = parser.parse_args()
+
+  _ = parse_input(args.infile.read())
