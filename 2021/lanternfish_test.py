5407a54f0c99b34c29ffd2165118489ae153614b
Will Angley <will@willangley.org>
2021-12-06 00:33:58 -0500

    [2021][Day 6] Lanternfish: part one!

--- 2021/lanternfish_test.py
+++ 2021/lanternfish_test.py
@@ -14,6 +14,19 @@ class TestCase(unittest.TestCase):
     self.assertListEqual([3, 4, 3, 1, 2],
                          lanternfish.parse_input(self.raw_input))
 
+  def test_simulate_day(self):
+    self.assertListEqual([2, 3, 2, 0, 1],
+                         lanternfish.simulate_day([3, 4, 3, 1, 2]))
+    self.assertListEqual([1, 2, 1, 6, 0, 8],
+                         lanternfish.simulate_day([2, 3, 2, 0, 1]))
+
+  def test_simulate(self):
+    self.assertEqual(26,
+                     lanternfish.simulate(lanternfish.parse_input("3,4,3,1,2"),
+                                          18))
+    self.assertEqual(5934,
+                     lanternfish.simulate(lanternfish.parse_input("3,4,3,1,2"),
+                                          80))
 
 if __name__ == '__main__':
   unittest.main()
