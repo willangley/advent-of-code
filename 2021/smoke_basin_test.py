8e6b0fc01efcc173efe374cd6532b27e4519e6dd
Will Angley <willangley@google.com>
2021-12-11 15:15:39 -0500

    [2021][Day 9] Smoke Basin: input parsing.

--- 2021/smoke_basin_test.py
+++ 2021/smoke_basin_test.py
@@ -9,10 +9,16 @@ import smoke_basin
 class SmokeBasinTestCase(unittest.TestCase):
   def setUp(self):
     self.raw_input = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
 """
 
   def test_parse_input(self):
-    self.assertListEqual([], smoke_basin.parse_input(self.raw_input))
+    self.assertListEqual([2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+                         smoke_basin.parse_input(self.raw_input)[0])
 
 
 if __name__ == '__main__':
