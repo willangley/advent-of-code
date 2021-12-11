07945d76d3878a8d7020d95337c4f84b90f2cf06
Will Angley <willangley@google.com>
2021-12-11 15:56:26 -0500

    [2021][Day 9] Smoke Basin: part two!

--- 2021/smoke_basin.py
+++ 2021/smoke_basin.py
@@ -5,9 +5,10 @@ https://adventofcode.com/2021/day/9
 """
 
 import argparse
+from collections import deque
 from dataclasses import dataclass
 import sys
-from typing import List
+from typing import Dict, List, Set
 
 
 def parse_input(raw_input: str) -> List[List[int]]:
@@ -16,7 +17,7 @@ def parse_input(raw_input: str) -> List[List[int]]:
           raw_input.strip().splitlines()]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Point:
   i: int
   j: int
@@ -51,6 +52,28 @@ def find_risk_level(heightmap: List[List[int]], points: List[Point]) -> int:
   return sum(1 + heightmap[p.i][p.j] for p in points)
 
 
+def find_basins(heightmap: List[List[int]]) -> Dict[Point, Set[Point]]:
+  basins = {lp: set() for lp in find_low_points(heightmap)}
+  for low_point in basins.keys():
+    queue = deque([low_point])
+    while len(queue):
+      cur = queue.popleft()
+      neighbors = {n for n in get_neighbors(heightmap, cur) if
+                   heightmap[n.i][n.j] < 9 and n not in basins[low_point]}
+      queue.extend(neighbors)
+      basins[low_point] |= neighbors
+  return basins
+
+
+def largest_basins(heightmap: List[List[int]]) -> int:
+  basins = find_basins(heightmap)
+  product = 1
+  for size in sorted(
+      [len(basin) for basin in basins.values()], reverse=True)[:3]:
+    product *= size
+  return product
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Day 9: Smoke Basin")
   parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
@@ -60,3 +83,4 @@ if __name__ == "__main__":
   heightmap = parse_input(args.infile.read())
   print('[Part 1] risk level:',
         find_risk_level(heightmap, find_low_points(heightmap)))
+  print('[Part 2] three largest basins:', largest_basins(heightmap))
