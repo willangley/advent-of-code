01f1e7f496fe9b9355c298d4330a5d4c5848f549
Will Angley <willangley@google.com>
2021-12-11 12:56:47 -0500

    [2021][Day 8] Seven Segment Search: part one!

--- 2021/seven_segment_search.py
+++ 2021/seven_segment_search.py
@@ -5,12 +5,24 @@ https://adventofcode.com/2021/day/8
 """
 
 import argparse
+from enum import Enum
 import sys
+from typing import List
+
+
+class Part(Enum):
+  ONE = 1
 
 
 def parse_input(raw_input: str):
-  for line in raw_input.strip().splitlines():
-    pass
+  return [[pattern.strip().split() for pattern in line.split('|')] for line in
+          raw_input.strip().splitlines()]
+
+
+def count_digits(entries: List[List[List[str]]], part: Part = Part.ONE):
+  return sum(
+      sum(1 for digit in display if len(digit) in {2, 3, 4, 7}) for _, display
+      in entries)
 
 
 if __name__ == "__main__":
@@ -19,4 +31,5 @@ if __name__ == "__main__":
                       default=sys.stdin)
   args = parser.parse_args()
 
-  _ = parse_input(args.infile.read())
+  entries = parse_input(args.infile.read())
+  print('[Part 1] digits 1, 4, 7, 8 appear:', count_digits(entries))
