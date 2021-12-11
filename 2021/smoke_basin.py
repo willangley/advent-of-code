8c198ee0e6398bba2a0c63db6647cf81ee2970b9
Will Angley <willangley@google.com>
2021-12-11 15:15:39 -0500

    [2021][Day 9] Smoke Basin: input parsing.

--- 2021/smoke_basin.py
+++ 2021/smoke_basin.py
@@ -6,11 +6,13 @@ https://adventofcode.com/2021/day/9
 
 import argparse
 import sys
+from typing import List
 
 
-def parse_input(raw_input: str):
-  for line in raw_input.strip().splitlines():
-    pass
+def parse_input(raw_input: str) -> List[List[int]]:
+  """Parses input into a heightmap."""
+  return [[int(c) for c in line.strip()] for line in
+          raw_input.strip().splitlines()]
 
 
 if __name__ == "__main__":
