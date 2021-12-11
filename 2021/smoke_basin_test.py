4cf920092618890b7eb576a5bcf8beefbb366aea
Will Angley <willangley@google.com>
2021-12-11 15:36:03 -0500

    [2021][Day 9] Smoke Basin: part one!

--- 2021/smoke_basin_test.py
+++ 2021/smoke_basin_test.py
@@ -20,6 +20,19 @@ class SmokeBasinTestCase(unittest.TestCase):
     self.assertListEqual([2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
                          smoke_basin.parse_input(self.raw_input)[0])
 
+  def test_find_low_points(self):
+    Point = smoke_basin.Point
+    self.assertListEqual([Point(0, 1), Point(0, 9), Point(2, 2), Point(4, 6)],
+                         smoke_basin.find_low_points(
+                             (smoke_basin.parse_input(self.raw_input))))
+
+  def test_find_risk_level(self):
+    heightmap = smoke_basin.parse_input(self.raw_input)
+    self.assertEqual(
+        15, smoke_basin.find_risk_level(heightmap,
+                                        smoke_basin.find_low_points(
+                                            heightmap)))
+
 
 if __name__ == '__main__':
   unittest.main()
