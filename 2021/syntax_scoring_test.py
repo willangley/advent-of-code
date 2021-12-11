b88551cbd85e9630649f0a08d8c796e751886aac
Will Angley <willangley@google.com>
2021-12-11 16:50:28 -0500

    [2021][Day 10] Syntax Scoring: parts one and two!

--- 2021/syntax_scoring_test.py
+++ 2021/syntax_scoring_test.py
@@ -6,14 +6,28 @@ import unittest
 import syntax_scoring
 
 
-class TestCase(unittest.TestCase):
+class SyntaxScoringTestCase(unittest.TestCase):
   def setUp(self):
     self.raw_input = """
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
 """
 
-  def test_parse_input(self):
-    self.assertListEqual([], syntax_scoring.parse_input(self.raw_input))
+  def test_score_file(self):
+    file = syntax_scoring.parse_input(self.raw_input)
+    self.assertEqual(26397, syntax_scoring.score_errors(file))
 
+  def test_score_completions(self):
+    file = syntax_scoring.parse_input(self.raw_input)
+    self.assertEqual(288957, syntax_scoring.score_completions(file))
 
 if __name__ == '__main__':
   unittest.main()
