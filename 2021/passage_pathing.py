fbf6713606210c2a1ac48b663d695dfcbb812693
Will Angley <will@willangley.org>
2022-04-23 21:41:42 +0000

    [2021][Day 12] Part two!

--- 2021/passage_pathing.py
+++ 2021/passage_pathing.py
@@ -6,6 +6,7 @@ https://adventofcode.com/2021/day/12
 
 import argparse
 import sys
+from typing import Callable
 
 
 import networkx as nx
@@ -28,24 +29,42 @@ def parse_input(raw_input: str) -> nx.DiGraph:
     return graph
 
 
-def dfs_impl(graph: nx.DiGraph, u: str, path: list[str]):
+CanVisit = Callable[[list[str], str], bool]
+
+
+def dfs_impl(graph: nx.DiGraph, u: str, path: list[str], can_visit: CanVisit):
     if u == 'end':
         yield path
 
     for v in graph[u]:
-        if v not in path or not v.islower():
+        if can_visit(path, v):
             path.append(v)
-            yield from dfs_impl(graph, v, path)
+            yield from dfs_impl(graph, v, path, can_visit)
             path.pop()
 
 
-def dfs(graph: nx.DiGraph):
+def dfs(graph: nx.DiGraph, can_visit: CanVisit):
     start = 'start'
-    return [list(path) for path in dfs_impl(graph, start, [start])]
+    return [list(path) for path in dfs_impl(graph, start, [start], can_visit)]
 
 
 def part_one(graph: nx.DiGraph):
-    return len(dfs(graph))
+    def can_visit(path: list[str], node: str) -> bool:
+        return node not in path or not node.islower()
+    return len(dfs(graph, can_visit))
+
+
+def part_two(graph: nx.DiGraph):
+    def can_visit(path: list[str], node: str) -> bool:
+        if not node.islower():
+            return True
+        if node not in path:
+            return True
+
+        small_caves = [p for p in path if p.islower()]
+        return len(small_caves) == len(set(small_caves))
+
+    return len(dfs(graph, can_visit))
 
 
 if __name__ == "__main__":
@@ -58,3 +77,4 @@ if __name__ == "__main__":
 
     graph = parse_input(args.infile.read())
     print('Part one: ', part_one(graph))
+    print('Part two: ', part_two(graph))
