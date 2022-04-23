f591f9b905a05e6432a6ad969163cab3ec4c219c
Will Angley <will@willangley.org>
2022-04-23 18:34:46 +0000

    [2021][Day 12] Input parsing.

--- 2021/passage_pathing_test.py
+++ 2021/passage_pathing_test.py
@@ -7,13 +7,29 @@ import passage_pathing
 
 
 class TestCase(unittest.TestCase):
-  def setUp(self):
-    self.raw_input = """
+
+    def setUp(self):
+        self.raw_input = """
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
 """
 
-  def test_parse_input(self):
-    self.assertListEqual([], passage_pathing.parse_input(self.raw_input))
+    def test_parse_input(self):
+        self.assertEqual(
+            {
+                'start': {'A', 'b'},
+                'A': {'start', 'end', 'c', 'b'},
+                'b': {'start', 'A', 'd', 'end'},
+                'c': {'A'},
+                'd': {'b'},
+                'end': {'A', 'b'}
+            }, passage_pathing.parse_input(self.raw_input))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
