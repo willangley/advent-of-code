752b01f99088556756a34a762eab2da016ca9b7e
Will Angley <will@willangley.org>
2022-04-23 18:37:52 +0000

    [2021][Day 12] Switch to four space indentation.

--- .idea/fileTemplates/Advent of Code puzzle.py.child.0.py
+++ .idea/fileTemplates/Advent of Code puzzle.py.child.0.py
@@ -7,13 +7,13 @@ import ${PUZZLE_FILENAME}
 
 
 class TestCase(unittest.TestCase):
-  def setUp(self):
-    self.raw_input = """
+    def setUp(self):
+        self.raw_input = """
 """
 
-  def test_parse_input(self):
-    self.assertListEqual([], ${PUZZLE_FILENAME}.parse_input(self.raw_input))
+    def test_parse_input(self):
+        self.assertListEqual([], ${PUZZLE_FILENAME}.parse_input(self.raw_input))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
